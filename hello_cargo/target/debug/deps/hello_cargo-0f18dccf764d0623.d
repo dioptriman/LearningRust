d40453db09291dfe1c30257d30fadfdb38feeeb5
andhika agung pratama <andhikaagung79@gmail.com>
2024-03-28 23:12:55 +0700

    Playing with Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-0f18dccf764d0623.d
@@ -0,0 +1,5 @@
+/Users/andhikaagungpratama/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-0f18dccf764d0623: src/main.rs
+
+/Users/andhikaagungpratama/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-0f18dccf764d0623.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-45b558d2ef6a7f96.d
@@ -0,0 +1,5 @@
+/Users/andhikaagungpratama/Documents/rust/hello_cargo/target/debug/deps/libhello_cargo-45b558d2ef6a7f96.rmeta: src/main.rs
+
+/Users/andhikaagungpratama/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-45b558d2ef6a7f96.d: src/main.rs
+
+src/main.rs:
